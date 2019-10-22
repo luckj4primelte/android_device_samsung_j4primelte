67d0cade30ff11812cc3116b3292078d72163d4b
Stargazer19 <pviraj19@gmail.com>
2019-10-22 15:20:21 +0530

    ignore neverallows

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -164,6 +164,7 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Shims
 TARGET_LD_SHIM_LIBS := \

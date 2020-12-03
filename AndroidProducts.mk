66fe9faca4314ecb02d9c8a2d5ea4a5b2ea4a744
Gabriel260 <adeilmazk@gmail.com>
2020-12-03 14:22:09 +0000

    ZenX

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,8 +14,8 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_j4primelte.mk
+    $(LOCAL_DIR)/zenx_j4primelte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_j4primelte-userdebug \
-    lineage_j4primelte-eng
+    zenx_j4primelte-userdebug \
+    zenx_j4primelte-eng
